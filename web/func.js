89e2f5e49b4be50eb04f5707798b4c8aa42735ae
Daniel Lee <daniel@uncountable.com>
2024-02-23 16:33:27 -0800

    basic web display working now

--- web/func.js
+++ web/func.js
@@ -1,11 +1,18 @@
-document.getElementById("runButton").addEventListener("click", () => {
-    const func = Module.cwrap("transpile", "string", ["string"])
-    console.log(func("x = 77"))
-    // const result = Module.ccall(
-    //   "transpile", // name of C function
-    //   [Number], // return type
-    //   [Number], // argument types
-    //   [2], // arguments
-    // );
-    // console.log(result)
-  });
+const transpile = (input) => {
+  const func = Module.cwrap("transpile", "string", ["string"])
+  try {
+    const result = func(input)
+    return result
+  } catch(error) {
+    return "syntax error"
+  }
+}
+
+const handleButtonClicked = () => {
+  const input = document.getElementById("input").value
+  const output = transpile(input)
+  console.log({output})
+  document.getElementById("output").innerText = output
+}
+
+document.getElementById("runButton").addEventListener("click", handleButtonClicked)
